b766a3011411fae4a18580ca9845ff63446195f8
beingborn <dolmin0708@naver.com>
2024-08-13 17:53:38 +0900

    클릭 시 백그라운드 컬러 바뀌게 하기

--- js/manage__menu.js
+++ js/manage__menu.js
@@ -5,13 +5,20 @@ $(document).ready(function() {
 
         // 클릭된 depth-1 항목의 부모(li 요소)를 active 클래스로 토글
         let $depth1Item = $(this).parent();
-
+        console.log($depth1Item)
         // 다른 depth-1 항목의 active 클래스 제거 및 depth-2 항목 숨기기
-        // $('.menu__depth-1').not($depth1Item).removeClass('active').find('.menu__depth-2').slideUp();
-
-        // 클릭된 항목의 depth-2 항목 슬라이드 토글 및 active 클래스 토글
-        $depth1Item.toggleClass('active').find('.menu__depth-2').slideToggle();
+        // $depth1Item.toggleClass('active').find('.menu__depth-2').slideToggle();
     });
+
+    $('.menu__depth-2 > .depth__2-title').click(function(e){
+        e.preventDefault();
+
+        let $depth2Item = $(this).parent();
+        console.log($depth2Item)
+
+        // $depth2Item.toggleClass('active').find('.menu__depth-3').slideToggle();
+    })
+
 });
 
 
@@ -19,11 +26,11 @@ $(document).ready(function() {
 // lnb height 기본값이 auto 여서 slideToggle 기능 수행 시 document가 함께 늘어남. 
 // 그래서 innerHeight을 이용해서 값을 유동적으로 지정해줘서 그 안에서만 slide되게 지정함
 
-$(document).ready(function(){
-    let lnb = $('.main__content .sub .lnb')
-    let lnbHeight = lnb.innerHeight();
-    lnb.css('height', lnbHeight)
-})
+// $(document).ready(function(){
+//     let menuWrap = $('.menu-list')
+//     let menuHeight = menuWrap.innerHeight();
+//     menuWrap.css('height', menuHeight)
+// })
 
 $(document).ready(function(){
     let tablePagination = $('.pagination .page-link') 
@@ -44,27 +51,15 @@ $(document).ready(function(){
     $('.menu__depth-2').click(function(e){
         e.preventDefault(); // 링크 기본 동작 방지
 
-        let currentIndex = $(this).index();
-        $(this).addClass("active");
-        $('.menu__depth-2').not($(this)).removeClass('active')
-        $('.auth__wrap').hide().eq(currentIndex).show();
-    })
+        // let currentIndex = $(this).index();
+        // $(this).addClass("active");
+        // $('.menu__depth-2').not($(this)).removeClass('active')
 
+    })
     $('.menu__depth-2').eq(0).addClass('active');
-    $('.auth__wrap').hide().eq(0).show();
 })
 
 
-/**
- * 콘텐츠간 높이 구하기 위해 
-
-        부모 wrap scrollHeight 값에서 해당 엘리먼트 값 간의 차이 구해서 
-        오른쪽 radio wrap들에게 동적으로 정확한 값 주기 // radio__wrap은 position absolute 처리되어있음
-        만약 depth2의 offsetTop을 뺄경우에는 - 2 값을 해주어야한다.
-
- * 
- * 
- */
 
 
 
@@ -170,6 +165,18 @@ $(document).ready(function(){
 
 })
    
+
+
+
+
+
+let menuAuth = $('.menu__auth-input') 
+
+console.log(menuAuth)
+
+$('.menu__auth-input').click(function(){
+    console.log("클릭되었습니다.")
+})
 
 
 
