8620efe25d43dea953783037f66881bbd12cdffd
beingborn <dolmin0708@naver.com>
2024-08-23 17:52:47 +0900

    메인페이지 수정 중

--- js/default.js
+++ js/default.js
@@ -1,6 +1,10 @@
 
 
 
+  $(document).ready(function(){
+    console.log("불러졌음")
+  })
+
   /**
    * 스크롤 이벤트 감지해서 헤더 고정 
    * or 숨기기
@@ -37,3 +41,45 @@
       lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // 스크롤이 0일 때 음수로 가지 않게 함
     });
   });
+
+
+  /**
+   * 헤더 영역 토글 함수
+   * 
+   * 
+   * 1. active 값에 따라 펼치기
+   * 
+   */
+
+  $(document).ready(function() {
+    const gnbTop__Menu = $('#gnb1 > .top__menu > li.plus > a');
+    const gnbSub__Menu = $('#gnb1 .sub__menu > ul > li > a');
+
+    gnbTop__Menu.click(function() {
+        const gnbOpenMenu = $(this).parent(); 
+        const isItOpen = gnbOpenMenu.hasClass('active');
+
+        if (isItOpen) {
+            gnbOpenMenu.removeClass('active');
+        } else {
+            // 모든 상위 메뉴에서 'active' 클래스 제거
+            gnbTop__Menu.parent().removeClass('active');
+            // 현재 클릭된 상위 메뉴에 'active' 클래스 추가
+            gnbOpenMenu.addClass('active');
+        }
+
+        const firstSub__MenuItem = gnbOpenMenu.find('.sub__menu ul li:first-child a');
+        if (firstSub__MenuItem.length > 0) {
+            gnbOpenMenu.siblings().find('.sub__menu ul li').removeClass('active');
+            firstSub__MenuItem.parent().addClass('active');
+        }
+        });
+
+        gnbSub__Menu.click(function() {
+        const gnbOpenSub__Menu = $(this).parent();
+        // 모든 서브메뉴에서 'active' 클래스 제거
+        gnbSub__Menu.parent().removeClass('active');
+        // 현재 클릭된 서브메뉴에 'active' 클래스 추가
+        gnbOpenSub__Menu.addClass('active');
+        });
+    });
